5fc2672449615bf2d3c5fdc3417302f04262fe39
Brendan Cowley <b.cowley1220@gmail.com>
2019-08-28 16:40:44 -0400

    'server is done'

--- shoppingServer/routes/shopping-cart.routes.js
+++ shoppingServer/routes/shopping-cart.routes.js
@@ -1,26 +1,60 @@
 const express = require("express");
 const shoppingRoutes = express.Router();
-const shoppingCart = require("./shopping-cart");
+let pool = require("../connections/pg-connection-pool");
 
-shoppingRoutes.get("/shoppingCart", (req, res) => {
-  res.send(shoppingCart);
-});
+function selectAllItems(req, res) {
+  pool.query("select * from shoppingcart order by id").then(result => {
+    res.send(result.rows);
+  });
+}
+
+shoppingRoutes.get("/shoppingCart", selectAllItems);
 
 shoppingRoutes.post("/shoppingCart", (req, res) => {
-  shoppingCart.push(req.body);
-  res.send(shoppingCart);
+  pool
+    .query(
+      "insert into shoppingcart (product, price, quantity) values ($1::text, $2::money, $3::int)",
+      [req.body.product, req.body.price, req.body.quantity]
+    )
+    .then(() => {
+      selectAllItems(req, res);
+    });
 });
 
 shoppingRoutes.put("/shoppingCart/:id", (req, res) => {
-  const index = shoppingCart.findIndex(person => person.id === req.params.id);
-  shoppingCart.splice(index, 1, req.body);
-  res.send(shoppingCart);
+  pool
+    .query("update shoppingcart set quantity=$1::int where id=$2::int", [
+      req.body.quantity,
+      req.params.id
+    ])
+    .then(() => {
+      selectAllItems(req, res);
+    });
 });
 
 shoppingRoutes.delete("/shoppingCart/:id", (req, res) => {
-  const index = shoppingCart.findIndex(person => person.id === req.params.id);
-  shoppingCart.splice(index, 1);
-  res.send(shoppingCart);
+  pool
+    .query("delete from shoppingcart where id=$1::int", [req.params.id])
+    .then(() => {
+      selectAllItems(req, res);
+    });
 });
+
+// shoppingRoutes.post("/shoppingCart", (req, res) => {
+//   shoppingCart.push(req.body);
+//   res.send(shoppingCart);
+// });
+
+// shoppingRoutes.put("/shoppingCart/:id", (req, res) => {
+//   const index = shoppingCart.findIndex(person => person.id === req.params.id);
+//   shoppingCart.splice(index, 1, req.body);
+//   res.send(shoppingCart);
+// });
+
+// shoppingRoutes.delete("/shoppingCart/:id", (req, res) => {
+//   const index = shoppingCart.findIndex(person => person.id === req.params.id);
+//   shoppingCart.splice(index, 1);
+//   res.send(shoppingCart);
+// });
 
 module.exports = shoppingRoutes;
